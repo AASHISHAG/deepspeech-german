24d81e052583425d0b7d88d61d81901b53d8fd9f
AASHISHAG <agarwalaashish20@gmail.com>
2019-09-13 14:33:33 +0200

    Updated Code

--- run_to_utf_8.sh
+++ run_to_utf_8.sh
@@ -2,13 +2,20 @@
 # file name: to_utf8
 
 FILES="/home/LTLab.lan/agarwal/german-speech-corpus/voxforge/*/etc/prompts-original"
+
 #FILES="/home/LTLab.lan/agarwal/german-speech-corpus/voxforge/*/etc/PROMPTS"
 
 for f in $FILES
 do
+  #filename="${f%.*}"
+  #echo -n "$f"
+  #file -I $f
   encoding=$(file -i "$f" | sed "s/.*charset=\(.*\)$/\1/")
+  #if file -I $f | grep -wq "iso-8859-1"
   if [  "${encoding}" = "iso-8859-1" ]
   then
+    #mkdir -p converted
+    #cp $f ./converted
     iconv -f ISO-8859-1 -t UTF-8 $f > "${f}_utf8.tex"
     mv "${f}_utf8.tex" $f
     echo ": CONVERTED TO UTF-8."
